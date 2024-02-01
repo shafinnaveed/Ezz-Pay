fa957e03d05bd36ab32e569a577f21dddfd45787
shafinnaveed <shafinnaveed13@gmail.com>
2024-02-01 20:23:35 +0500

    phase 1

--- app/src/main/java/com/example/ezzpay/SettingActivity.kt
+++ app/src/main/java/com/example/ezzpay/SettingActivity.kt
@@ -11,7 +11,7 @@ import com.google.firebase.database.*
 class SettingActivity : AppCompatActivity() {
 
     private lateinit var mobileNumberEditText: EditText
-    private lateinit var emailEditText: EditText
+
     private lateinit var updateButton: Button
     private lateinit var databaseReference: DatabaseReference
 
@@ -20,7 +20,6 @@ class SettingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_setting)
 
         mobileNumberEditText = findViewById(R.id.mobileNumberEditText)
-        //emailEditText = findViewById(R.id.emailEditText)
         updateButton = findViewById(R.id.updateButton)
 
         // Initialize the database reference
@@ -34,35 +33,20 @@ class SettingActivity : AppCompatActivity() {
 
     private fun updateSettings() {
         val newMobileNumber = mobileNumberEditText.text.toString().trim()
-        val newEmail = emailEditText.text.toString().trim()
 
         // Validate input
-        if (newMobileNumber.isEmpty() && newEmail.isEmpty()) {
+        if (newMobileNumber.isEmpty()) {
             Toast.makeText(this, "Please enter new mobile number", Toast.LENGTH_SHORT).show()
             return
         }
 
         // Update mobile number if provided
         if (newMobileNumber.isNotEmpty()) {
-            databaseReference.child("mobileNumber").setValue(newMobileNumber)
+            databaseReference.child("number").setValue(newMobileNumber)
         }
 
         // Update email if provided
-        if (newEmail.isNotEmpty()) {
-            // Update email in Firebase Realtime Database
-            databaseReference.child("email").setValue(newEmail)
 
-            // Update email in Firebase Authentication
-            val currentUser = FirebaseAuth.getInstance().currentUser
-            currentUser?.updateEmail(newEmail)
-                ?.addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(this, "Email updated successfully.", Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(this, "Failed to update email.", Toast.LENGTH_SHORT).show()
-                    }
-                }
-        }
 
         Toast.makeText(this, "Settings updated successfully.", Toast.LENGTH_SHORT).show()
         finish()
